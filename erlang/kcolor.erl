6c9f1ea060463590b1210225f400a7ce4df90699
Casper Strömberg <casper@barkh.se>
2020-11-10 11:57:33 +0100

    adding property tests and improving kcolor

--- erlang/kcolor.erl
+++ erlang/kcolor.erl
@@ -1,12 +1,18 @@
 -module(kcolor).
 
+-include_lib("proper/include/proper.hrl").
+
+-include_lib("eunit/include/eunit.hrl").
+
 -export([addEdge/3,
          addNode/2,
-         kcolor/2,
+         completeConnectedGraph/1,
+         createGraph/0,
+         getAdjecentNodes/2,
          getNodes/1,
-         isSafeColor/3]).
-
-
+         graphFromNodes/1,
+         isSafeColor/3,
+         kcolor/2]).
 
 isSafeColor(Neighbors, NodesWithColor, Color) ->
     NeighborsColors = lists:filtermap(fun ({L, C}) ->
@@ -19,35 +25,52 @@ isSafeColor(Neighbors, NodesWithColor, Color) ->
                                       NodesWithColor),
     lists:member(Color, NeighborsColors) == false.
 
-kcolor(Graph,MaxColors) -> kcolor(Graph, [],MaxColors, 0).
+kcolor(Graph, MaxColors) ->
+    kcolor(Graph, [], MaxColors, 0).
 
-kcolor(_, _,MaxColors, CurrentColor) when CurrentColor > MaxColors -> false;
-kcolor([], NodesWithColor,_, _) -> NodesWithColor;
-kcolor([Head | Tail], NodesWithColor,MaxColors, CurrentColor) ->
-
+kcolor(_, _, MaxColors, CurrentColor)
+    when CurrentColor > MaxColors ->
+    false;
+kcolor([], NodesWithColor, _, _) ->
+    lists:reverse(lists:map(fun ({Label, Color}) ->
+                                    {Label, [Color + $a]}
+                            end,
+                            NodesWithColor));
+kcolor([Head | Tail], NodesWithColor, MaxColors,
+       CurrentColor) ->
     {Label, Neighbors} = Head,
     IsSafe = isSafeColor(Neighbors,
                          NodesWithColor,
                          CurrentColor),
     if IsSafe == true ->
            kcolor(Tail,
-                      [{Label, CurrentColor}] ++ NodesWithColor,MaxColors,
-                      0);
+                  [{Label, CurrentColor}] ++ NodesWithColor,
+                  MaxColors,
+                  0);
        IsSafe == false ->
            kcolor([Head | Tail],
-                      NodesWithColor,MaxColors,
-                      CurrentColor + 1)
+                  NodesWithColor,
+                  MaxColors,
+                  CurrentColor + 1)
     end.
 
-addNode(Graph, Label) ->
-    Graph ++ [{Label, []}].
+createGraph() -> [].
+
+addNode(Graph, Label) -> Graph ++ [{Label, []}].
+
+getAdjecentNodes([Node | Tail], LabelA) ->
+    case Node of
+        {Label, Neighbors} when Label == LabelA -> Neighbors;
+        _Else -> getAdjecentNodes(Tail, LabelA)
+    end.
 
 addEdgeAux(Node, LabelA, LabelB) ->
     case Node of
         {Label, Neighbors} when Label == LabelA ->
-            {Label, Neighbors ++ [LabelB], 0};
+            {Label, Neighbors ++ [LabelB]};
         {Label, Neighbors} when Label == LabelB ->
-            {Label, Neighbors ++ [LabelA], 0}
+            {Label, Neighbors ++ [LabelA]};
+        _Else -> Node
     end.
 
 addEdge(Graph, LabelA, LabelB) ->
@@ -57,3 +80,59 @@ addEdge(Graph, LabelA, LabelB) ->
 
 getNodes(Graph) ->
     lists:map(fun ({Label, _}) -> Label end, Graph).
+
+%% TESTS
+
+%% remove duplicates taken from slides
+remove_duplicates([]) -> [];
+remove_duplicates([A | T]) ->
+    case lists:member(A, T) of
+        true -> remove_duplicates(T);
+        false -> [A | remove_duplicates(T)]
+    end.
+
+list_no_dupls(T) ->
+    ?LET(L, (list(T)), (remove_duplicates(L))).
+
+graphFromNodes(Arr) ->
+    Graph = createGraph(),
+    lists:foldr(fun (Elem, Acc) -> addNode(Acc, Elem) end,
+                Graph,
+                Arr).
+
+completeConnectedGraph(Graph) ->
+    Nodes = getNodes(Graph),
+    completeConnectedGraph(Nodes, Graph).
+
+completeConnectedGraph([], Graph) -> Graph;
+completeConnectedGraph([A | Tail], Graph) ->
+    completeConnectedGraph(Tail,
+                           lists:foldr(fun (B, Acc) -> addEdge(Acc, A, B) end,
+                                       Graph,
+                                       Tail)).
+
+node_lengh_prop() ->
+    ?FORALL(L, (list_no_dupls(pos_integer())),
+            (length(getNodes(graphFromNodes(L))) =:= length(L))).
+
+node_lengh_prop_test() ->
+    ?assertEqual(true,
+                 (proper:quickcheck(node_lengh_prop(),
+                                    [{to_file, user}]))).
+
+adjecent_complete_prop() ->
+    ?FORALL(L, (list_no_dupls(pos_integer())),
+            (lists:all(fun (El) ->
+                               lists:sort(getAdjecentNodes(completeConnectedGraph(graphFromNodes(L)),
+                                                           El))
+                                   =:=
+                                   lists:sort(lists:filter(fun (A) -> A /= El
+                                                           end,
+                                                           L))
+                       end,
+                       L))).
+
+adjecent_complete_prop_test() ->
+    ?assertEqual(true,
+                 (proper:quickcheck(adjecent_complete_prop(),
+                                    [{to_file, user}]))).
--- erlang/perm.erl
+++ erlang/perm.erl
@@ -1,12 +1,9 @@
 %% File: hello.erl
 -module(perm).
 
--export([contains_231/1,
-         perm/2,
-         prop_same_input_pass/0]).
+-export([perm/2]).
 
 -include_lib("proper/include/proper.hrl").
-
 -include_lib("eunit/include/eunit.hrl").
 
 perm(Inp, Out) ->
@@ -32,17 +29,28 @@ isStackPermutation([CurrentVal | NextInput],
                               [CurrentVal] ++ [TopOfStack | RestStack])
     end.
 
-prop_same_input_pass() ->
-    ?FORALL(L, (orderedlist(pos_integer())), (perm(L, L))).
+insert_231(Arr) ->
+    Len = length(Arr),
+    Pos = trunc(math:floor(rand:uniform() * Len)),
+    {Left, Right} = lists:split(Pos, Arr),
+    Left ++ [2, 3, 1] ++ Right.
 
 list_no_dupls_inserted_231(T) ->
-    ?LET(L, (list(T)), (remove_duplicates(insert_231(L)))).
+    ?LET(L, (list(T)), (insert_231(remove_123(L)))).
+
+remove_123([]) -> [];
+remove_123([A | T]) ->
+    case A of
+        1 -> remove_123(T);
+        2 -> remove_123(T);
+        3 -> remove_123(T);
+        _Else -> [A | remove_123(T)]
+    end.
 
 list_no_dupls(T) ->
     ?LET(L, (list(T)), (remove_duplicates(L))).
 
-%% better versions of remove_duplicates/1 exist
-
+%% remove duplicates taken from slides
 remove_duplicates([]) -> [];
 remove_duplicates([A | T]) ->
     case lists:member(A, T) of
@@ -50,30 +58,53 @@ remove_duplicates([A | T]) ->
         false -> [A | remove_duplicates(T)]
     end.
 
-insert_231(Arr) ->
-    Len = length(Arr),
-    Pos = trunc(math:floor(rand:uniform() * Len)),
-    {Left, Right} = lists:split(Pos, Arr),
-    Left ++ [2, 3, 1] ++ Right.
-
+%% Checks if a sequence contains a 231 pattern
 contains_231([]) -> false;
-contains_231([_]) -> false;
-contains_231([_, _]) -> false;
-contains_231([X, Y, Z | Tail]) ->
-    IsPrefix = Z < X andalso X < Y,
-    if IsPrefix -> true;
-       true -> contains_231([X, Y, Z | Tail])
+contains_231(Arr) ->
+    Max = lists:max(Arr),
+    {A, B} = lists:splitwith(fun (A) -> A /= Max end, Arr),
+    IsCont = length(A) /= 0 andalso
+                 length(B) /= 0 andalso
+                     lists:max(A) < Max andalso lists:min(B) < lists:max(A),
+    if IsCont -> true;
+       true ->
+           contains_231(lists:filter(fun (El) -> El /= Max end,
+                                     Arr))
     end.
+
+prop_same_input_same_output() ->
+    ?FORALL(L, (orderedlist(pos_integer())), (perm(L, L))).
+
+prop_same_input_same_output_test() ->
+    ?assertEqual(true,
+                 (proper:quickcheck(prop_same_input_same_output(),
+                                    [{to_file, user}]))).
 
 prop_231_false() ->
     ?FORALL(L, (list_no_dupls_inserted_231(pos_integer())),
-            (?IMPLIES((contains_231(L)),
-                      (false == perm(lists:sort(L), L))))).
+            (false == perm(lists:sort(L), L))).
+
+prop_231_false_test() ->
+    ?assertEqual(true,
+                 (proper:quickcheck(prop_231_false(),
+                                    [{to_file, user}]))).
 
 prop_231_avoiding_true() ->
     ?FORALL(L, (list_no_dupls(pos_integer())),
             (?IMPLIES((contains_231(L) == false),
                       (true == perm(lists:sort(L), L))))).
+
+prop_231_avoiding_true_test() ->
+    ?assertEqual(true,
+                 (proper:quickcheck(prop_231_avoiding_true(),
+                                    [{to_file, user}]))).
+
+perm_basic_231_pattern_should_return_false_test() ->
+    false = perm([1, 2, 3, 4, 5, 6], [1, 2, 4, 5, 3, 6]),
+    false = perm([1, 2, 3, 4, 5, 6], [2, 3, 1, 4, 5, 6]),
+    false = perm([1, 2, 3, 4, 5, 6], [2, 5, 6, 3, 4, 1]),
+    false = perm([1, 2, 3, 4, 5, 6], [5, 1, 2, 4, 6, 3]),
+    ok.
 
 perm_basic_test() ->
     true = perm([1, 2, 3], [3, 1, 2]),
