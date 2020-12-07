a564e92c30fac9f7948f2869fc2c4fe0cb576e2d
Casper Strömberg <casper@barkh.se>
2020-12-07 12:34:05 +0100

    add racket perm

---
+++ racket/perm.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(provide (all-defined-out))
+
+(define/match (isStackPermutation inp out stack)
+  [('() out stack) (equal? out stack)]
+  [(inp y '()) (isStackPermutation (rest inp) y (list (first inp)))]
+  [(inp out stack) 
+    (
+      if (equal? (first out) (first stack)) 
+        (isStackPermutation inp (rest out) (rest stack)) 
+        (isStackPermutation (rest inp) out (cons (first inp) stack)
+      )
+    )
+  ])
+
+(define (perm inp out) (isStackPermutation (reverse inp) (reverse out) '()))
+
+(write (perm '(1 2 3) '(1 2 3)))
